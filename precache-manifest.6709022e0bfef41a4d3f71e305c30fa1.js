ada63a2d9e90753b30fcf6d6aaed847a01e096de
karhik hegde <karthikhegde55146@gmail.com>
2024-03-21 11:56:38 +0530

    Updates

---
+++ precache-manifest.6709022e0bfef41a4d3f71e305c30fa1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5cee8ec24d4db1bf53934436cd48448",
+    "url": "/TextUtils-React/index.html"
+  },
+  {
+    "revision": "2350c2c1aca2045d46a5",
+    "url": "/TextUtils-React/static/css/main.1ded7047.chunk.css"
+  },
+  {
+    "revision": "337c03a67066b62d2f6d",
+    "url": "/TextUtils-React/static/js/2.39596e5b.chunk.js"
+  },
+  {
+    "revision": "99562942502e41af72fc73d84afe4332",
+    "url": "/TextUtils-React/static/js/2.39596e5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00690b6ffdf2ed259a16",
+    "url": "/TextUtils-React/static/js/3.286ae661.chunk.js"
+  },
+  {
+    "revision": "2350c2c1aca2045d46a5",
+    "url": "/TextUtils-React/static/js/main.563300af.chunk.js"
+  },
+  {
+    "revision": "600d78fd7cbb6b8ea585",
+    "url": "/TextUtils-React/static/js/runtime-main.35608f1d.js"
+  }
+]);
